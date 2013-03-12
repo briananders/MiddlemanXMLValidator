8052c4f72f0c0ba7ecfc0a24da6894be9bc95443
briananders <anders.brian@icloud.com>
2013-03-12 12:23:44 -0500

    Initial Commit

---
+++ config.rb
@@ -0,0 +1,100 @@
+###
+# Compass
+###
+
+# Susy grids in Compass
+# First: gem install susy
+# require 'susy'
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy (fake) files
+# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
+#   @which_fake_page = "Rendering a fake page with a variable"
+# end
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+def validate(document_path, schema_path, root_element)
+    schema = Nokogiri::XML::Schema(File.read(schema_path))
+    document = Nokogiri::XML(File.read(document_path))
+    schema.validate(document.xpath("//#{root_element}").to_s)
+end
+
+
+
+module MyFeature
+    class << self
+        def registered(app)
+            app.after_build do |builder|
+                puts "--After Build--"
+            end
+        end
+        alias :included :registered
+    end
+end
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+    # For example, change the Compass output style for deployment
+    # activate :minify_css
+
+    # Minify Javascript on build
+    # activate :minify_javascript
+
+    # Enable cache buster
+    # activate :cache_buster
+
+    # Use relative URLs
+    # activate :relative_assets
+
+    # Compress PNGs after build
+    # First: gem install middleman-smusher
+    # require "middleman-smusher"
+    # activate :smusher
+
+    # Or use a different image path
+    # set :http_path, "/Content/images/"
+
+    puts "--Before Build--"
+
+    activate MyFeature
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XMLValidate::Application.config.secret_token = '377a7f6c8c5862bbd16734d9971fe5500f3c8122231ec56141f7d2ed45a3421388fffd784a46014e8592d143be9d52eeda6c61e915002d222f7d8b268ca71aad'
