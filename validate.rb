8be04fb93c43d0705835d064209002820524f6f7
briananders <anders.brian@icloud.com>
2013-03-12 23:03:59 -0500

    added loops to find all .rss and .xml files in the build folder

--- validate.rb
+++ validate.rb
@@ -4,7 +4,15 @@ module MyFeature
     class << self
         def registered(app)
             app.after_build do |builder|
-                puts "--After Build--"
+                puts "--After Build--",""
+                Dir.glob("build/*.xml").each do |full_path|
+                	puts "   Validating.....  " + full_path
+                	
+                end
+                Dir.glob("build/*.rss").each do |full_path|
+                	puts "   Validating.....  " + full_path
+                	
+                end
             end
         end
         alias :included :registered
