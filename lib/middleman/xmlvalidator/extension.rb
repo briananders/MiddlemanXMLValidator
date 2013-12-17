cd690172e073b802a1750f8f73a5f7a3faa92b6a
Brent Coughenour <bcoughenour@nestlabs.com>
2013-12-16 16:39:18 -0800

    further refactoring for xml validator

--- lib/middleman/xmlvalidator/extension.rb
+++ lib/middleman/xmlvalidator/extension.rb
@@ -6,29 +6,28 @@ module Middleman
 
 				app.after_build do |builder|
 					puts "", "Validating with NokoGiri", ""
-					p Xmlvalidator.files_to_validate
 
-					Xmlvalidator.files_to_validate.each do |full_path|
-						file_name = full_path.split('/').last
-						validator_file = file_name.gsub(/\.\w*/, '.xsd')
-						validator_filepath = File.join(File.dirname(__FILE__), '/schema/' + validator_file)
-						validator = File.exists?(validator_filepath) ? validator_file : (file_name.end_with?('.rss') ? "RSSSchema.xsd" : "XMLSchema.xsd")
-
-						puts "  validating".blue + "  #{full_path}....." + (Xmlvalidator.valid(full_path, validator) == true ? "COMPLETE".green : "ERRORS FOUND".red)
-						Xmlvalidator.validate(full_path, validator).each do |error|
-							puts "     " + error.message
-						end
+					Dir.glob("build/**/*.{xml,rss}").each do |full_path|
+						Xmlvalidator.validate_file(full_path)
 					end
 
 					puts "", "Validation with NokoGiri " + "Complete".green, ""
 				end
 			end
 
-			def files_to_validate
-				Dir.glob("build/**/*.{xml,rss}")
-			end
-
 			alias :included :registered
+		end
+
+		def self.validate_file(file_path)
+			file_name = full_path.split('/').last
+			validator_file = file_name.gsub(/\.\w*/, '.xsd')
+			validator_filepath = File.join(File.dirname(__FILE__), '/schema/' + validator_file)
+			validator = File.exists?(validator_filepath) ? validator_file : (file_name.end_with?('.rss') ? "RSSSchema.xsd" : "XMLSchema.xsd")
+
+			puts "  validating".blue + "  #{full_path}....." + (Xmlvalidator.valid(full_path, validator) == true ? "COMPLETE".green : "ERRORS FOUND".red)
+			Xmlvalidator.validate(full_path, validator).each do |error|
+				puts "     " + error.message
+			end
 		end
 
 		def self.validate(document_path, schema_path)
